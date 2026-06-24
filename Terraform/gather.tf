4d8059d9013b8435fec0153dcbeeacd26fcc8c85
flamefaiz <faizmatnoor97@gmail.com>
2026-06-24 15:46:25 +0800

    Initial commit for Netflix-clone repository

---
+++ Terraform/gather.tf
@@ -0,0 +1,22 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+
+  filter {
+    name = "name"
+    values = [
+      "ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*",
+      "ubuntu/images/hvm/ubuntu-noble-24.04-amd64-server-*",
+      "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-*",
+      "ubuntu/images/hvm-ebs-gp3/ubuntu-noble-24.04-amd64-server-*"
+    ]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  #Look for images only owned by official Ubuntu Account
+  owners = ["099720109477"]
+}
---
+++ Terraform/vpc.tf
@@ -0,0 +1,80 @@
+locals {
+  org     = "faiz"
+  project = "netflix-clone"
+  env     = var.env
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.cidr-block
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${local.org}=${local.project}-${local.env}-vpc"
+    Env  = "${local.env}"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${local.org}=${local.project}-${local.env}-igw"
+    env  = var.env
+  }
+
+  depends_on = [aws_vpc.vpc]
+}
+
+resource "aws_subnet" "public-subnet" {
+  count                   = var.pub-subnet-count
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = element(var.pub-cidr-block.count.index)
+  availability_zone       = element(var.pub-availability-zone, count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${local.org}-${local.project}-${local.env}-public-subnet-${count.index + 1}"
+    env  = var.env
+
+  }
+
+  depends_on = [aws_vpc.vpc]
+
+}
+
+resource "aws_route_table_association" "public_rta" {
+  count          = 4
+  route_table_id = aws_route_table.public-rt.id
+  subnet_id      = aws_subnet.public-subnet[count.index].id
+
+  depends_on = [aws_vpc.vpc, aws_subnet.public-subnet]
+}
+
+resource "aws_security_group" "default-ec2-sg" {
+  name        = "${local.org}-${local.project}-${local.env}-sg"
+  description = "Default Security Group"
+
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = "[0.0.0.0/0]"
+  }
+
+  tags = {
+    name = "${local.org}-${local.project}-${local.env}-sg"
+
+  }
+}
