0271a0aa7fa73e4abea4b10d769b2247dc367dfb
Muhammad Faiz <77822431+flamefaiz@users.noreply.github.com>
2026-06-24 16:35:04 +0800

    Update Terraform required version and AWS provider version

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~>1.13.3"
+  required_version = ">=1.13.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.14.1"
+      version = "6.14.1"
     }
   }
   cloud {}
